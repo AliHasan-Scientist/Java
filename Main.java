d7c82a81d54a27f8fc78416c578e685a8a419987
Roy Ali Hasan <informativepath0987@gmail.com>
2021-10-28 16:55:42 -0700

    "Add condition "

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Ali Hasan";
+        System.out.println(name);
+        float myNum = 1329.65f;
+        int myNum2 = 1329;
+        boolean change = true;
+        System.out.println(change);
+        System.out.println(myNum);
+        System.out.println(myNum2);
+        //
+        if (change = false) {
+            System.out.println("You can login");
+
+        }
+        else{
+            System.out.println("You can't Login");
+        }
+    }
+}
